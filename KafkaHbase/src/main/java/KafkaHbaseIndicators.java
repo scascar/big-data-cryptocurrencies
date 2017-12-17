288b81a93a9c1f9048faf5b126392c33bd5510c8
Oscar Blazejewski* <oscar_blazejewski@yahoo.fr>
2017-12-17 20:59:37 +0100

    Splitted into two classes for the two tables and consumers

--- KafkaHbase/src/main/java/KafkaHbaseIndicators.java
+++ KafkaHbase/src/main/java/KafkaHbaseIndicators.java
@@ -23,7 +23,7 @@ import java.util.Arrays;
 import org.apache.hadoop.hbase.util.Bytes;
 
 
-public class KafkaHbase {
+public class KafkaHbaseIndicators {
 
     public static void main(String[] args) throws Exception {
 
@@ -37,10 +37,10 @@ public class KafkaHbase {
 		properties.put("value.deserializer", StringDeserializer.class.getName());
 		properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 		
-		//Creates two consumers for the candles and indicators
-		KafkaConsumer<String,String> candleConsumer = new KafkaConsumer<String,String>(properties);
+		//Creates consumer for the indicators
+		KafkaConsumer<String,String> indicatorConsumer = new KafkaConsumer<String,String>(properties);
 		
-		candleConsumer.subscribe(Arrays.asList("streaming.bitfinex.candles"));
+		indicatorConsumer.subscribe(Arrays.asList("streaming.bitfinex.indicators"));
 		
 
 		//Hbase Config
@@ -49,33 +49,29 @@ public class KafkaHbase {
 		Connection conn = ConnectionFactory.createConnection(config);
 
 		//Tables to write on: candles and indicators
-	 	Table candleTable = conn.getTable(TableName.valueOf("bitfinex.candles"));
+		Table indicatorTable = conn.getTable(TableName.valueOf("bitfinex.indicators"));
 
 		int candleRowkey = 0;
 		int indicatorRowkey = 0;
 
 		while(true){
 			
-			ConsumerRecords<String,String> candleRecords = candleConsumer.poll(100);
-			if(!candleRecords.isEmpty()){
-					for(ConsumerRecord<String,String> record: candleRecords){
-						
+
+			ConsumerRecords<String,String> indicatorRecords = indicatorConsumer.poll(100);
+			if(!indicatorRecords.isEmpty()){
+					for(ConsumerRecord<String,String> record: indicatorRecords){
+
 						String[] values = record.value().split(",");
 						System.out.println(record.value());
-						Put valuePut = new Put(Bytes.toBytes(Integer.toString(candleRowkey)));
-						valuePut.add(Bytes.toBytes("values"),Bytes.toBytes("channel_id"), Bytes.toBytes(values[0]));
+						Put valuePut = new Put(Bytes.toBytes(Integer.toString(indicatorRowkey)));
+						valuePut.add(Bytes.toBytes("values"),Bytes.toBytes("indicator"), Bytes.toBytes(values[0]));
 						valuePut.add(Bytes.toBytes("values"),Bytes.toBytes("timestamp"), Bytes.toBytes(values[1]));
-						valuePut.add(Bytes.toBytes("values"),Bytes.toBytes("open"), Bytes.toBytes(values[2]));
-						valuePut.add(Bytes.toBytes("values"),Bytes.toBytes("close"), Bytes.toBytes(values[3]));
-						valuePut.add(Bytes.toBytes("values"),Bytes.toBytes("high"), Bytes.toBytes(values[4]));
-						valuePut.add(Bytes.toBytes("values"),Bytes.toBytes("low"), Bytes.toBytes(values[5]));
-						valuePut.add(Bytes.toBytes("values"),Bytes.toBytes("volume"), Bytes.toBytes(values[6]));
-						candleTable.put(valuePut);
+						valuePut.add(Bytes.toBytes("values"),Bytes.toBytes("value"), Bytes.toBytes(values[2]));
+						indicatorTable.put(valuePut);
 
-						candleRowkey++;
+						indicatorRowkey++;
 					}
 			}
-			else{System.out.println("candleRecords empty");}
 
 		Thread.sleep(10000);
 	}
